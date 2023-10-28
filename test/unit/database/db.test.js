1b22aa3e02c6d4b1abd5cb8973955624ae0afe31
danuja01 <it21023378@my.sliit.lk>
2023-10-28 14:52:50 +0530

    added tests

---
+++ test/unit/database/db.test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+// ES6 Promises
+mongoose.Promise = global.Promise;
+
+// Connect to the database before running tests
+before(function (done) {
+  this.timeout(10000); // Set timeout to 10 seconds
+
+  // Connect to MongoDB
+  mongoose.connect(process.env.MONGO_TEST_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connection has been made.');
+      done();
+    })
+    .on('error', (error) => {
+      console.log('Connection error:', error);
+      done(error);
+    });
+});
+
+describe('Database connection', () => {
+  it('should connect to the database', () => {
+    expect(mongoose.connection.readyState).to.equal(1);
+  });
+});
